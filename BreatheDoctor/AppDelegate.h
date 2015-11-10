91ab37228954696f79a8502d5cc61e325e02c261
SeaLwh <1057726968@qq.com>
2015-11-10 12:14:32 +0800

    Initial Commit

---
+++ BreatheDoctor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BreatheDoctor
+//
+//  Created by comv on 15/11/10.
+//  Copyright © 2015年 lwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BreatheDoctor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BreatheDoctor
+//
+//  Created by comv on 15/11/10.
+//  Copyright © 2015年 lwh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
